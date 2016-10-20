018590a0281ad13698c0365c58a7f225a437bbec
Chris Wingo and Quinn Schwartz <quinn@>
2016-10-20 15:08:12 -0700

    error checking is hard

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,36 @@ $(document).ready(function() {
   $("form#celebrity").submit(function(event){
     //UI Logic
 
+    var pass = false;
+    var age = parseInt($("input#age").val());
+    var name = $("input#name").val();
 
+    if(!pass) {
+      // if("#name" && "#age"){
+      //   $("#nameInput").hide();
+      //   $("#nameInputError").show();
+      //   $("#ageInput").hide();
+      //   $("#ageInputError").show();
+      // }
+      debugger;
+      if(!age) {
+        alert("Age is blank")
+        $("#ageInput").hide();
+        $("#ageInputError").show();
+        } else {
+        $("#ageInputSuccess").show();
+        $("#ageInput").hide();
+      }
+      if(!name) {
+        alert("Name is blank")
+        $("#nameInput").hide();
+        $("#nameInputError").show();
+        } else {
+        $("nameInputSuccess").show();
+      }
+    } else {
+      pass = true;
+    }
 
     //Business Logic
 
